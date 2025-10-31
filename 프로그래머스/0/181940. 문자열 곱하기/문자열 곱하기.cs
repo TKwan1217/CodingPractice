0ccd6bee2fdac825995ebc54379fc1dfaebd517d
TKwan1217 <101793699+TKwan1217@users.noreply.github.com>
2025-10-31 10:33:54 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.33 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -1,11 +1,13 @@
-using System;
+using System.Text;
 
 public class Solution {
     public string solution(string my_string, int k) {
-        string answer = "";
+        StringBuilder answer = new StringBuilder();
+        
         for (int i = 0; i < k; i++){
-        answer += my_string;
+            answer.Append(my_string);
         }
-        return answer;
+        
+        return answer.ToString();
     }
 }
