ec18ebbe5a5772b710ce8fa731ec351ed93c8635
TKwan1217 <101793699+TKwan1217@users.noreply.github.com>
2025-11-01 22:13:32 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = n; i >= 1; i -= 2){
+            answer += i % 2 == 0 ? i*i : i;
+        }
+        return answer;
+    }
+}
