2bd8686ea56e23afcfe16105a51ebc526e31cf27
TKwan1217 <101793699+TKwan1217@users.noreply.github.com>
2025-11-02 10:47:56 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int multiple = 1;
+            
+        for(int i = 0; i < num_list.Length; i++){
+            sum += num_list[i];
+            multiple *= num_list[i];
+        }
+        
+        answer = multiple < sum*sum ? 1 : 0;
+            
+        return answer;
+    }
+}
