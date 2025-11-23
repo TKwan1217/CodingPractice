ebda078be1086868f49f2745cbdca73b635a32f9
TKwan1217 <101793699+TKwan1217@users.noreply.github.com>
2025-11-23 16:10:34 +0900

    [level 1] Title: 소수 찾기, Time: 6.27 ms, Memory: 31.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -14,8 +14,9 @@ public class Solution {
         //Ex) 16 = 1*16, 2*8 , 4*4, 8*2, 16*1
         //Ex) 5 = 1*5, root5 * root5, 5*1 
         //가운데 제곱근까지만 구하면 대칭되는 지 확인 가능
-        
-        for(int i = 2; i <= Math.Sqrt(n)+1; i++){
+        //제곱근 + 에라토스테네스의 체 알고리즘
+        //에라토스테네스의 체 알고리즘 > i 부터 제곱근 까지 Loop > 현재 i의 배수들을 소수에서 제거
+        for(int i = 2; i <= Math.Sqrt(n); i++){
             if(prime[i]){
                 int j = 2;
                 while(i * j <= n){
