79a167601d3651fdb2a9c8d3ce281a248a0b0a6e
TKwan1217 <101793699+TKwan1217@users.noreply.github.com>
2025-11-23 16:17:12 +0900

    [level 1] Title: 소수 찾기, Time: 193.79 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -4,31 +4,20 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        //배열 칸과 실제 for문 i 맞추기 위해 n+1
-        bool[] prime = new bool[n+1];
-        
         for(int i = 2; i <= n; i++){
-            prime[i] = true;
-        }
-        
-        //Ex) 16 = 1*16, 2*8 , 4*4, 8*2, 16*1
-        //Ex) 5 = 1*5, root5 * root5, 5*1 
-        //가운데 제곱근까지만 구하면 대칭되는 지 확인 가능
-        //제곱근 + 에라토스테네스의 체 알고리즘
-        //에라토스테네스의 체 알고리즘 > i 부터 제곱근 까지 Loop > 현재 i의 배수들을 소수에서 제거
-        for(int i = 2; i <= Math.Sqrt(n); i++){
-            if(prime[i]){
-                int j = 2;
-                while(i * j <= n){
-                    prime[i*j] = false;
-                    j++;
+            bool IsPrime = true;
+            int nr = (int)Math.Sqrt(i);
+
+            for(int j = 2; j <= nr; j++){
+                if(i % j == 0){
+                    IsPrime = false;
+                    break;
                 }
             }
-        }
-        
-        for(int i = 2; i <= n; i++){
-            if(prime[i])
+            
+            if(IsPrime)
                 answer++;
+            
         }
         
         return answer;
